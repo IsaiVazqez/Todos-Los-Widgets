9e63b7037dd503e0e2d49b8a72ee23ff0278d5dc
Isaí Vázquez Valdez <7yuvryi@gmail.com>
2022-05-09 17:41:19 -0500

    const

--- lib/widgets/custom_bottom_navigation.dart
+++ lib/widgets/custom_bottom_navigation.dart
@@ -1,29 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       showSelectedLabels: false,
       showUnselectedLabels: false,
       selectedItemColor: Colors.pink,
-      backgroundColor: Color.fromRGBO(55, 57, 84, 1),
-      unselectedItemColor: Color.fromRGBO(116, 117, 152, 1),
+      backgroundColor: const Color.fromRGBO(55, 57, 84, 1),
+      unselectedItemColor: const Color.fromRGBO(116, 117, 152, 1),
       currentIndex: 0,
-      items: [
+      items: const [
         BottomNavigationBarItem(
-          icon: Icon( Icons.calendar_today_outlined ),
-          label: 'Calendario'
-        ),
+            icon: Icon(Icons.calendar_today_outlined), label: 'Calendario'),
         BottomNavigationBarItem(
-          icon: Icon( Icons.pie_chart_outline_outlined ),
-          label: 'Gráfica'
-        ),
+            icon: Icon(Icons.pie_chart_outline_outlined), label: 'Gráfica'),
         BottomNavigationBarItem(
-          icon: Icon( Icons.supervised_user_circle_outlined ),
-          label: 'Usuarios'
-        ),
+            icon: Icon(Icons.supervised_user_circle_outlined),
+            label: 'Usuarios'),
       ],
     );
   }
